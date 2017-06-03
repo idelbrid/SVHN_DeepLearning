aa924963862be135419e48c1d2ef54615b10e6d9
Ian Delbridge <idelbrid@gmail.com>
2017-06-02 21:37:35 -0400

    adding svhn utilities for preprocessing

---
+++ svhn_utils.py
@@ -0,0 +1,58 @@
+import PIL
+import numpy as np
+# import h5py
+
+
+def file2tensor(filename):
+    with PIL.Image.open(filename) as im:
+        width = im.size[0]
+        if im.size[1] < width // 2:
+            cropped = im.crop([0, 0, width, width // 2])
+        else:
+            cropped = im.crop([0, 0, im.size[1]*2, im.size[1]])
+        resized = cropped.resize([64, 32])
+        return np.asarray(resized)
+
+
+def parse_digit_struct(digit_struct):
+    def maybe_deref(ref):
+        if isinstance(ref, float):
+            return ref
+        else:
+            return digit_struct[ref][0,0]
+
+    def parse_bboxes(bboxes_obj):
+        bboxes = []
+        for href, lref, leref, tref, wref in zip(bboxes_obj['height'], bboxes_obj['label'], bboxes_obj['left'], bboxes_obj['top'], bboxes_obj['width']):
+            bbox = dict()
+            bbox['height'] = maybe_deref(href[0])
+            bbox['width'] = maybe_deref(wref[0])
+            bbox['length'] = maybe_deref(leref[0])
+            bbox['top'] = maybe_deref(tref[0])
+            bbox['label'] = maybe_deref(lref[0])
+            bboxes.append(bbox)
+        return bboxes
+
+    digitstructdict = dict()
+    for bbox_list, name in zip(digit_struct['digitStruct']['bbox'], digit_struct['digitStruct']['name']):
+        bboxes_obj = digit_struct[bbox_list[0]]
+        names_obj = digit_struct[name[0]]
+
+        name = ''.join(chr(i) for i in names_obj)
+        bboxes = parse_bboxes(bboxes_obj)
+
+        digitstructdict[name] = bboxes
+    return digitstructdict
+
+
+def one_hot(dig):
+    toreturn = np.zeros([len(dig), 10])
+    for i in range(len(dig)):
+        toreturn[i, dig[i]] = 1
+    return toreturn
+
+
+def pad_one_hots(oh):
+    toreturn = np.zeros((5, 10))
+    toreturn[:len(oh), :] = oh
+    return toreturn
